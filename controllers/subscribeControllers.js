1814b65e94a29b25fa8b9a57fba06d732cbb1816
KhatuRao <ankit3289003@gmail.com>
2025-07-25 15:53:58 +0530

    change subscribe controller

--- controllers/subscribeControllers.js
+++ controllers/subscribeControllers.js
@@ -23,7 +23,7 @@ export const subscribe = async (req, res) => {
         const transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: Number(process.env.SMTP_PORT),
-            secure: Number(process.env.SMTP_PORT),
+            secure: Number(process.env.SMTP_PORT) === 465,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
